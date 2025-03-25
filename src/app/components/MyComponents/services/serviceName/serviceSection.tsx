792812b96845d20706d4b6f6d1619c2bf0de0ad9
Trilok <tiosuthar@gmail.com>
2025-03-25 12:16:19 +0530

    update code

--- src/app/components/MyComponents/services/serviceName/serviceSection.tsx
+++ src/app/components/MyComponents/services/serviceName/serviceSection.tsx
@@ -22,15 +22,15 @@ export default function ServicesSection() {
       </div>
       <section className="py-12 transition-colors duration-300 bg-white flex flex-col items-center ">
         <div className="container px-6">
-          <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-7 gap-6 place-items-center">
-            {services.map((service, index) => (
-              <ServiceCard
-                key={index}
-                title={service.title}
-                imageSrc={service.imageSrc}
-              />
-            ))}
-          </div>
+            <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-7 gap-6 place-items-center">
+              {services.map((service, index) => (
+                <ServiceCard
+                  key={index}
+                  title={service.title}
+                  imageSrc={service.imageSrc}
+                />
+              ))}
+            </div>
         </div>
       </section>
     </section>
